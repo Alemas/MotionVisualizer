009d14e34bc1e967b2ce5c78099332dc9b594abc
Alemas <mateusreckziegel@gmail.com>
2015-08-06 19:18:41 -0300

    Initial Commit

---
+++ MotionVisualizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MotionVisualizer
+//
+//  Created by Mateus Reckziegel on 8/6/15.
+//  Copyright (c) 2015 Mateus Reckziegel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
