c30d8b5c4bb8935dd9c5fca1ccb9e1206e67ce4a
Alemas <mateusreckziegel@gmail.com>
2015-08-13 17:12:25 -0300

    first commit

---
+++ MotionVisualizer/CustomView.swift
@@ -0,0 +1,19 @@
+//
+//  CustomView.swift
+//  MotionVisualizer
+//
+//  Created by Mateus Reckziegel on 8/13/15.
+//  Copyright (c) 2015 Mateus Reckziegel. All rights reserved.
+//
+
+import Cocoa
+
+class CustomView: NSView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
